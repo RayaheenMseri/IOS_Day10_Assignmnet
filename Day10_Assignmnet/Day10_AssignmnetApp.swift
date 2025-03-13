601148cd22ae72ea0567454f3c472e39d310559a
RayaheenMseri <rayaheenmseri@gmail.com>
2025-03-13 12:54:01 +0300

    Initial Commit

---
+++ Day10_Assignmnet/Day10_AssignmnetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day10_AssignmnetApp.swift
+//  Day10_Assignmnet
+//
+//  Created by Taibah Valley Academy on 13/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Day10_AssignmnetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
